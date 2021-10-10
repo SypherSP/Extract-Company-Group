b620af258268bf629437043fac1828b326625db6
sypher-SP <72958686+SypherSP@users.noreply.github.com>
2021-10-10 18:03:31 +0530

    Restructured directories
    also added communication details on output screen

---
+++ CompanyGroup/code/Get_Group_of_Companies.py
@@ -0,0 +1,44 @@
+
+from os import write
+from bs4 import BeautifulSoup
+import requests
+
+list_of_companies = []
+html_text = ""
+
+def takeInput():
+    global html_text
+
+    print("Enter the URL of the zaubacorp search result (https://www.zaubacorp.com/) : ")
+    URL = input('>').strip()
+    html_text = requests.get(URL).text
+
+def getList():
+    global list_of_companies
+
+    soup = BeautifulSoup(html_text, 'lxml')
+    hidden_rows = soup.find_all('td', class_='hiddenRow')
+    a_tags = []
+    for row in hidden_rows:
+        a_tags.extend(row.find_all('a'))
+    for tag in a_tags:
+        if tag.text != 'Login':
+            list_of_companies.append(tag.text)
+    list_of_companies = set(list_of_companies)
+
+def writeToFile():
+    with open("group_companies.txt", 'w') as file:
+        for company in list_of_companies:
+            file.write(f"{company}\n")
+
+
+def main():
+    takeInput()
+    getList()
+    writeToFile()
+    print("List of companies can be viewed in \'group_companies.txt\' file\nTASK COMPLETED")
+    input("\nPress enter to exit...")
+
+
+if __name__ == "__main__":
+    main()
